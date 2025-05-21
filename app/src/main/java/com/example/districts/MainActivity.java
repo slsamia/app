ac5d99d21ad127acd7ad57347929078d1339dd49
samia lutfe <samia634572@gmail.com>
2025-05-21 10:47:19 +0600

    added

--- app/src/main/java/com/example/districts/MainActivity.java
+++ app/src/main/java/com/example/districts/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -27,46 +29,55 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         this.setTitle("districts");
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Button buttonT=findViewById(R.id.jashoreButton);
+
+        Button buttonT = findViewById(R.id.jashoreButton);
         buttonT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Jashore.class);
+                Intent intent = new Intent(MainActivity.this, Jashore.class);
                 startActivity(intent);
             }
         });
-        Button buttonMagura=findViewById(R.id.maguraButton);
+        Button buttonMagura = findViewById(R.id.maguraButton);
         buttonMagura.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, Magura.class);
+                Intent intent = new Intent(MainActivity.this, Magura.class);
                 startActivity(intent);
             }
         });
-        Button buttonKhulna=findViewById(R.id.khulnaButton);
+        Button buttonKhulna = findViewById(R.id.khulnaButton);
         buttonKhulna.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, Khulna.class);
+                Intent intent = new Intent(MainActivity.this, Khulna.class);
                 startActivity(intent);
             }
         });
-        Button buttonJhenaidah=findViewById(R.id.jhenaidahButton);
+        Button buttonJhenaidah = findViewById(R.id.jhenaidahButton);
         buttonJhenaidah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, Jhenaidah.class);
+                Intent intent = new Intent(MainActivity.this, Jhenaidah.class);
                 startActivity(intent);
             }
         });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.dot_menu,menu);
+        inflater.inflate(R.menu.dot_menu, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (R.id.dot_about == id) {
+            Intent intent = new Intent(MainActivity.this, Test.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
